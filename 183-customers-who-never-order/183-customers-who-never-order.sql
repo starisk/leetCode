d1224da7bdac382042245606f3513b9832105ad4
starisk <thestarisk@gmail.com>
2022-08-13 21:41:59 +0900

    Time: 582 ms (49.60%), Space: 0B (100.00%) - LeetHub

--- 183-customers-who-never-order/183-customers-who-never-order.sql
+++ 183-customers-who-never-order/183-customers-who-never-order.sql
@@ -1,5 +1,6 @@
 # Write your MySQL query statement below
 SELECT NAME AS Customers
-  FROM CUSTOMERS
- WHERE ID NOT IN (SELECT DISTINCT CUSTOMERID
-                    FROM ORDERS)
+  FROM CUSTOMERS A
+ WHERE NOT EXISTS (SELECT * 
+                     FROM ORDERS B
+                    WHERE A.ID = B.CUSTOMERID)
