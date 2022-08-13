f0002c97026b1bb06f3d21fbdf9bbfb25825aba5
starisk <thestarisk@gmail.com>
2022-08-13 21:43:56 +0900

    Time: 816 ms (32.19%), Space: 0B (100.00%) - LeetHub

--- 183-customers-who-never-order/183-customers-who-never-order.sql
+++ 183-customers-who-never-order/183-customers-who-never-order.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
 SELECT NAME AS Customers
   FROM CUSTOMERS A
- WHERE NOT EXISTS (SELECT * 
-                     FROM ORDERS B
-                    WHERE A.ID = B.CUSTOMERID)
+  LEFT OUTER JOIN ORDERS B
+    ON A.ID = B.CUSTOMERID
+ WHERE B.ID IS NULL
